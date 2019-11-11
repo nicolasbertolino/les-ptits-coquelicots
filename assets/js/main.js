c1de9ea7370684f1b67cb1c76856dc7dc79a6fe0
Nicolas Bertolino <bertolinonicolas@gmail.com>
2019-11-11 11:48:43 +0100

    Change color

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,10 @@ document.addEventListener("mousemove", e => {
   cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
 });
 
+document.addEventListener("wheel", e => {
+  cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
+});
+
 link.addEventListener("mouseenter", e => {
   cursor.classList.toggle("expand");
 });

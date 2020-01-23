721d70b7a361b86a9fe52e292f8f5e5ef9eea638
Nicolas Bertolino <bertolinonicolas@gmail.com>
2020-01-23 07:35:20 +0100

    Modify cursor circle

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,11 +10,11 @@ window.onload = function () {
 const cursor = document.querySelector(".cursor");
 
 document.addEventListener("mousemove", e => {
-  cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
+  cursor.setAttribute("style", "top: " + (e.pageY - 17) + "px; left: " + (e.pageX - 17) + "px;")
 });
 
 document.addEventListener("wheel", e => {
-  cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
+  cursor.setAttribute("style", "top: " + (e.pageY - 17) + "px; left: " + (e.pageX - 17) + "px;")
 });
 
 document.querySelectorAll("a").forEach((link) => {

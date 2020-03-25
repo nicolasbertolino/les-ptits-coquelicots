63336d14b39672db4d47f9e96fb04c3043366328
Nicolas Bertolino <bertolinonicolas@gmail.com>
2020-03-25 13:35:03 +0100

    Separate barba js from main js

---
+++ assets/js/pagestransition.js
@@ -0,0 +1,43 @@
+function leaveTransition() {
+    gsap.to("header", { duration: 0.3, opacity: 0, ease: "power3" });
+    gsap.to("main", { duration: 0.3, opacity: 0, ease: "power3" });
+  }
+  
+  function contentAnimation() {
+    gsap.from("header div:first-child", { duration: 0.5, opacity: 0, ease: "power3" });
+    gsap.to("header div:last-child", { duration: 1, clipPath:"polygon(0 0, 100% 0, 100% 100%, 0 100%)", ease: "power3" });
+    gsap.from("main", { duration: 0.5, opacity: 0, ease: "power3" });
+  }
+  
+  function delay(n) {
+    n = n || 1000;
+    return new Promise(done => {
+      setTimeout(() => {
+        done();
+      }, n);
+    });
+  }
+  
+  barba.init({
+    sync: true,
+  
+    transitions: [
+      {
+        async leave(data) {
+          const done = this.async();
+  
+          leaveTransition();
+          await delay(400);
+          done();
+        },
+  
+        async enter(data) {
+          contentAnimation();
+        },
+  
+        async once(data) {
+          contentAnimation();
+        }
+      }
+    ]
+  });
